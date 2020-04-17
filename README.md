9a4ffa173e4707ddc499bcdefecbf7992ac93ce3
MadiGabrielle <marianafinamori@Marianas-MBP-2.lan1>
2020-04-17 14:16:07 -0400

    Changed style, fixedjs, changed html, wrote readme

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Psychic-Game
+OVERVIEW
+In this assignment, students should create a Psychic Game. These app will run in the browser, and feature dynamically updated HTML and CSS powered by JavaScript code.
+
+HOW TO PLAY
+Player types ramdon letters on the key board in order to guess the letter the app ramdomly picked. 
+Iniatilly the player has 10 chances to guess the letter. As the player type in letters, the wrong letters will be shown on the "guesses so far" and the guesses left will decrease.
+When the player guess the ramdon letter, increase the Wins counter and start the game over again (without refreshing the page).
+When the player loses, increase the Losses counter and restart the game without a page refresh (just like when the user wins).
+
+WHAT WE SHOULD SEE ON THE SCREEN
+Guess what letter I'm thinking of
+Wins: (# of times the user has guessed the letter correctly)
+Losses: (# of times the user has failed to guess the letter correctly after exhausting all guesses)
+Guesses Left: (# of guesses left. This will update)
+Your Guesses So Far: (the specific letters that the user typed. Display these until the user either wins or loses.)
+
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,47 +1,70 @@
+var wins = 0;
+var losses = 0;
+var guessesLeft = 10;
+var wrongGuesses = [];
+var computerChoice;
 
-        var wins = 0;
-        var losses = 0;
-        var guessesLeft = 10;
-        var wrongGuesses = [];
-
-        var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var computerChoices = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
     
-
-        var winsText = document.getElementById("wins-text");
-        var lossesText = document.getElementById("losses-text");
-        var guessesLeftText = document.getElementById("guesses-left-text");
-        var wrongLettersText = document.getElementById("wrong-letters-text");
-
+var winsText = document.getElementById("wins-text");
+var lossesText = document.getElementById("losses-text");
+var guessesLeftText = document.getElementById("guesses-left-text");
+var wrongLettersText = document.getElementById("wrong-letters-text");
+var alert = document.querySelector('.alert');
+var wrongLetter;
     
-        document.onkeyup = function(event) {
-        var userGuess = event.key;
-        var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-           
-
-            if (userGuess === computerGuess) {
-                wins++;
-                guessesLeft = 10;
-                wrongGuesses = [];
-                console.log(wins);
-            } 
-            else {
-                guessesLeft--;
-                wrongGuesses.push(userGuess);
-                console.log(wrongGuesses);
-                console.log(guessesLeft);
-            }    
-
-            if (guessesLeft === 0) {
-                losses++;
-                guessesLeft = 10;
-                wrongGuesses = [];
-                console.log(losses);
-            }
+//App ramdonly picks a letter
+function ramdonLetter() {
+    computerChoice = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+    console.log(computerChoice)
         
-            winsText.textContent = wins;
-            lossesText.innerHTML = losses;
-            guessesLeftText.innerHTML = guessesLeft;
-            wrongLettersText.innerHTML = wrongGuesses;
-            console.log(guessesLeft);
+    //Function compares user's guess and computer's choice
+    document.onkeyup = function(event) {
             
-        };
+        var userGuess = event.key.toUpperCase();
+        console.log("inside " + computerChoice)
+ 
+        if (userGuess === computerChoice && computerChoices.includes(userGuess)) {
+            wins++;
+            guessesLeft = 10;
+            wrongGuesses = [];
+            wrongLettersText.innerHTML = " ";
+            console.log(wins);
+            ramdonLetter();
+        } else if (userGuess != computerChoice && computerChoices.includes(userGuess)) {
+            guessesLeft--;
+            wrongGuesses.push(userGuess);
+            wrongGuesses.forEach((wrongGuess, index)=> {
+                wrongLetter = document.createElement('div');
+                wrongLetter.setAttribute("class", "wrongLetter");
+                wrongLetter.append(wrongGuess)
+            })
+            wrongLettersText.append(wrongLetter);
+            console.log(wrongGuesses);
+        }    
+
+        if (guessesLeft === 0) {
+            losses++;
+            guessesLeft = 10;
+            wrongGuesses = [];
+            console.log(losses);
+            document.querySelector(".wrongLetter").remove()
+            wrongLettersText.innerHTML = " ";
+            console.log("worked?")
+            ramdonLetter();
+        }
+
+        if (computerChoices.indexOf(userGuess) == -1) {
+            alert.classList.add('error');
+            alert.innerHTML = "You need to type only letters";
+            setTimeout(() => {
+                alert.innerHTML = " ";
+                alert.classList.remove('error')
+            }, 2000);
+        }
+        
+        winsText.textContent = wins;
+        lossesText.innerHTML = losses;
+        guessesLeftText.innerHTML = guessesLeft;   
+    }
+}
